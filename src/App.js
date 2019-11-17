6bb2a260b4596578cd2d7b6f806b38cedf744270
Alexander Gonzalez <alexgonzo1994@gmail.com>
2019-11-16 18:25:46 -0600

    updated app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import SignInandSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { dispatch } from "rxjs/internal/observable/pairs";
 import { setCurrentUser } from "./redux/user/user.actions";
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -44,15 +43,28 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInandSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,10 +3,10 @@ import SignIn from "../../components/sign-in/sign-in.component";
 import SignUp from "../../components/sign-up/sign-up.components";
 import "./sign-in-and-sign-up.styles.scss";
 
-const SignInandSignUpPage = () => (
+const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
     <SignUp />
   </div>
 );
-export default SignInandSignUpPage;
+export default SignInAndSignUpPage;
