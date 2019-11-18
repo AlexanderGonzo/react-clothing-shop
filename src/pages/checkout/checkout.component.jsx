02ec22afd7b5bc3e34a84b7332c389fe77bff758
Alexander Gonzalez <alexgonzo1994@gmail.com>
2019-11-18 00:10:26 -0600

    added stripe checkout to application

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+
 import {
   selectCartItems,
   selectCartTotal
@@ -31,9 +34,13 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Test Card Information*
+      <br />
+      4242 4242 4242 4242 - EXP: 01/20 - CVV: 123
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
