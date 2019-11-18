ee53e84da8e865782671b18720ef179de0762d6e
Alexander Gonzalez <alexgonzo1994@gmail.com>
2019-11-17 18:03:40 -0600

    changed shop_data to an object

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
+
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
@@ -17,11 +18,11 @@ const CollectionPage = ({ collection }) => {
         ))}
       </div>
     </div>
-)
+  );
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionid)(state)
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
 });
 
 export default connect(mapStateToProps)(CollectionPage);
