df9e06106d16da9a526df647fd2e324e6ebae701
Alexander Gonzalez <alexgonzo1994@gmail.com>
2019-11-20 10:28:29 -0600

    added floor to total

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,17 +35,19 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="sub-total">CART TOTAL: ${total}</div>
-    <div className="sub-total">TOTAL TAX: ${total * 0.8}</div>
-    <div className="sub-total">TOTAL SHIPPING: ${total * 0.3}</div>
+    <div className="sub-total">TOTAL TAX: ${Math.floor(total * 0.8)}</div>
+    <div className="sub-total">TOTAL SHIPPING: ${Math.floor(total * 0.3)}</div>
     <div className="total">
-      ORDER TOTAL: ${total + total * 0.8 + total * 0.3}
+      ORDER TOTAL: ${Math.floor(total + total * 0.8 + total * 0.3)}
     </div>
     <div className="test-warning">
       *Test Card Information*
       <br />
       4242 4242 4242 4242 - EXP: 01/20 - CVV: 123
     </div>
-    <StripeCheckoutButton price={total + total * 0.8 + total * 0.3} />
+    <StripeCheckoutButton
+      price={Math.floor(total + total * 0.8 + total * 0.3)}
+    />
   </div>
 );
 
